d7d22b08ad74cfa01376725ecbb5dba40aa8c312
semtrip <75387322+semtrip@users.noreply.github.com>
2022-07-05 04:20:39 +0700

    bug fix

--- src/components/MarquizWidget.js
+++ src/components/MarquizWidget.js
@@ -4,7 +4,7 @@ function MarquizWifget() {
    const Marquiz = window.Marquiz;
    useEffect(() => {
       (function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Widget', {id: '61115c5c12e050003e77c479', position: 'right', delay: 1})
-   })
+   }, [])
    return (
     console.log("Marquiz Widget Load")
    );

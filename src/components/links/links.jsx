dc9ba17d2fd9ecf7febf882d2211819fb0676d5d
semtrip <75387322+semtrip@users.noreply.github.com>
2022-07-06 12:56:42 +0700

    eject react

--- src/components/links/links.jsx
+++ src/components/links/links.jsx
@@ -15,19 +15,19 @@ function Links() {
                             </a>
                             <a href="https://грав-щебень.рф/" target="_blank" rel="noopener noreferrer">
                                 <img src="links_img/2.png" alt="https://изв-щебень.рф/" />
-                                <span>Известняковый щебень <i>от 520₽/м³</i></span>
+                                <span>Гравийный щебень <i>от 520₽/м³</i></span>
                             </a>
                             <a href="https://щебень-гранитный.рф/" target="_blank" rel="noopener noreferrer">
                                 <img src="links_img/3.png" alt="https://изв-щебень.рф/" />
-                                <span>Известняковый щебень <i>от 520₽/м³</i></span>
+                                <span>Гранитный щебень <i>от 520₽/м³</i></span>
                             </a>
                             <a href="https://pesok-ass.ru/" target="_blank" rel="noopener noreferrer">
                                 <img src="links_img/4.png" alt="https://изв-щебень.рф/" />
-                                <span>Известняковый щебень <i>от 520₽/м³</i></span>
+                                <span>Песок <i>от 520₽/м³</i></span>
                             </a>
                             <a href="https://keramzit-ass.ru/" target="_blank" rel="noopener noreferrer">
                                 <img src="links_img/5.png" alt="https://изв-щебень.рф/" />
-                                <span>Известняковый щебень <i>от 520₽/м³</i></span>
+                                <span>Керамзит <i>от 520₽/м³</i></span>
                             </a>
                         </div>
                     </div>
